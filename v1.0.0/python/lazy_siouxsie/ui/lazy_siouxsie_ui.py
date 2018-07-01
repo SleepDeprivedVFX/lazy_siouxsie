accced758572ec4ea218247b820dfc9c84d72dbb
Adam Benson <AdamBenson.vfx@gmail.com>
2018-06-30 18:21:58 -0700

    Got this to work in Shotgun.  Had to move it to manual, and shift a few folders around.

--- v1.0.0/python/lazy_siouxsie/ui/lazy_siouxsie_ui.py
+++ v1.0.0/python/lazy_siouxsie/ui/lazy_siouxsie_ui.py
@@ -7,7 +7,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PySide import QtCore, QtGui
+from sgtk.platform.qt import QtCore, QtGui
 
 class Ui_lazySiouxsie(object):
     def setupUi(self, lazySiouxsie):
